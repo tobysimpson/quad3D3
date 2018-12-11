a094433bcb8e8f93fd441aab0bbb67a2f005030b
Toby Simpson <toby@tobys-imac.local>
2018-12-11 10:30:18 +0100

    Initial Commit

---
+++ quad3D3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  quad3D3
+//
+//  Created by Toby Simpson on 11.12.18.
+//  Copyright © 2018 Toby Simpson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
