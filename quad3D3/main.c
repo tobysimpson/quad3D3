e4b675dc382eff586d15d37ce8595e41b7f3304e
Toby Simpson <toby@tobys-imac.mobile.usilu.net>
2018-12-11 10:31:59 +0100

    begun

--- quad3D3/main.c
+++ quad3D3/main.c
@@ -8,8 +8,12 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int main(int argc, const char * argv[])
+{
+    printf("hello\n");
+    
+    printf("done\n");
+    
     return 0;
 }
